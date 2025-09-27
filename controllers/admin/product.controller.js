f041814612c978e2942a8392df173b6655fb5a0e
Ho Hoang Son <23521352@gm.uit.edu.vn>
2025-09-28 04:04:39 +0700

    Import data

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,14 @@
-module.exports.product = (req, res) => {
+const Product = require("../../models/product.model");
+
+
+module.exports.product = async (req, res) => {
+    const products = await Product.find({
+        deleted: false, // to check not deleted products
+        status: "active" // to check active products only
+    });
+    console.log(products);
     res.render("admin/pages/product/index.pug", {
-        pageTitle: "Admin Product Page"
+        pageTitle: "Admin Product Page",
+        products: products
     });
 }
