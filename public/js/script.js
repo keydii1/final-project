735685dd62f3398579f44ab35c04e934f4601a60
Ho Hoang Son <23521352@gm.uit.edu.vn>
2025-09-28 17:04:45 +0700

    haf path for create search features

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,22 @@ if(buttonStatus.length>0){
      } );
 });
 }
+
+
+// form search
+const formSearch = document.querySelector(".form-search");
+if(formSearch){
+    let url = new URL (window.location.href);
+    formSearch.addEventListener("submit", (e)=>{
+        console.log(e);
+        // to prevent the default action of the form(reloading the page)
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+        if(keyword){
+            url.searchParams.set("keyword", keyword);
+        }else {
+            url.searchParams.delete("keyword");
+        }
+    });
+}  
+// end of form search
