2c43f3a0787f3988a4f667afca33ba0f771eb247
Ho Hoang Son <23521352@gm.uit.edu.vn>
2025-10-06 20:15:58 +0700

    Đẩy file tĩnh lên cloud

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -124,7 +124,6 @@ module.exports.createPost = async(req,res) => {
         req.body.position = countProducts;
     }
     else{ req.body.position = parseInt(req.body.position) || 0;}
-    if(req.file) req.body.thumbnail = `/uploads/${req.file.filename}`;
     const newProduct = new Product(req.body);
     await newProduct.save();
     res.redirect(`${systemconfig.prefixAdmin}/products`);
--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -1,28 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const multer  = require('multer')
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() })
+const multer = require('multer')
+
+
+// const storageMulter = require("../../helpers/storageMulter");
+// const upload = multer({ storage: storageMulter() })
+const upload = multer() // Sử dụng bộ nhớ tạm thời
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 const validate = require("../../validates/admin/product.validate");
 const controller = require("../../controllers/admin/product.controller");
-    router.get("/", controller.index);
-    router.patch("/change-status/:status/:id", controller.changeStatus);
-    router.patch("/change-multi", controller.changeMulti);
-    router.delete("/delete/:id", controller.deleteItem);
-    router.get("/create", controller.create);
-    router.post("/create",
-        upload.single("thumbnail"),
-        validate.titleIsNotNull,
-        validate.tileNoLessThan8character,
-        controller.createPost
-        );
-    router.get("/edit/:id", controller.edit);
-    router.patch("/edit/:id",
-        upload.single("thumbnail"),
-        validate.titleIsNotNull,
-        // validate.tileNoLessThan8character,
-        controller.editPatch);
-    router.get("/detail/:id", controller.detail);
+router.get("/", controller.index);
+router.patch("/change-status/:status/:id", controller.changeStatus);
+router.patch("/change-multi", controller.changeMulti);
+router.delete("/delete/:id", controller.deleteItem);
+router.get("/create", controller.create);
+router.post("/create",
+    upload.single("thumbnail"),
+    uploadCloud.uploadToCloud,
+    validate.titleIsNotNull,
+    validate.tileNoLessThan8character,
+    controller.createPost
+);
+router.get("/edit/:id", controller.edit);
+router.patch("/edit/:id",
+    upload.single("thumbnail"),
+    uploadCloud.uploadToCloud,
+    validate.titleIsNotNull,
+    // validate.tileNoLessThan8character,
+    controller.editPatch);
+router.get("/detail/:id", controller.detail);
 // Export router
 module.exports = router;
-
